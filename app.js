f4152f5762818670b4b607ab5b480fe09e02cad7
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-06-02 21:42:42 -0400

    Displaying title and content of blog post on home page

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,8 +13,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function (req, res) {
-  res.render("home", {homeContent: homeStartingContent, posts: posts});
+  res.render("home", {
+    homePageContent: homeStartingContent, 
+    posts: posts });
 });
 
 app.get("/about", function (req, res) {
@@ -34,7 +36,7 @@ app.get("/compose", function (req, res) {
 app.post("/compose", function (req, res) {
   const post = {
     title: req.body.postTitle, 
-    body: req.body.postBody,
+    content: req.body.postContent,
   };
   posts.push(post);
   res.redirect("/");
