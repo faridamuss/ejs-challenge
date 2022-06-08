38b36ce5e4bd05156c3d8f377a018741f8b9bf9d
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-06-07 22:52:48 -0400

    Create a schema for posts

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ async function main() {
   await mongoose.connect('mongodb://localhost:27017/postDB');
 }
 
+// creating SCHEMA for our posts
+const postSchema = {
+  title: String, 
+  content: String
+}
 
 
 let posts = [];
